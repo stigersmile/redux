3f375d41455684c1b95df9725cfccc2637d34cab
peter <stigersmile@gmail.com>
2022-03-09 12:15:42 +0800

    v1.0

---
+++ 03-starting-project/src/store/cart-slice.js
@@ -0,0 +1,47 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState:{
+        items:[],
+        totalQuantity:0
+    },
+    reducers:{
+        addItemToCart(state,action){
+            console.log("addItem")
+            const newItem = action.payload;
+            console.log(newItem)
+            const existingItem = state.items.find((item)=>item.id === newItem.id)
+            state.totalQuantity++;
+            if(!existingItem){
+                state.items.push({
+                    id:newItem.id,
+                    price:newItem.price,
+                    quantity: 1,
+                    totalPrice:newItem.price,
+                    name:newItem.title
+                })
+            }else{
+                existingItem.quantity++;
+                existingItem.totalPrice= existingItem.totalPrice + newItem.price;
+
+            }
+        },
+        removeItemFromCart(state,action){
+            console.log("removeItem")
+            const id= action.payload;
+            state.totalQuantity--;
+            const existingItem = state.items.find((item)=>item.id === id)
+            if(existingItem.quantity ===1){
+                state.items = state.items.filter((item)=>item.id !==id)
+            }else{
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
+        
+    }
+})
+
+export const cartActions = cartSlice.actions
+export default cartSlice;
